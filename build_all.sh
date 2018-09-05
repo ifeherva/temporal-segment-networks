9e677a3565b6cd066a692977792c483d8f7e4c5a
Istvan Fehervari <gooksl@gmail.com>
2018-09-05 07:56:29 -0700

    Removed Caffe build

--- build_all.sh
+++ build_all.sh
@@ -45,20 +45,20 @@ else
 fi
 
 # build caffe
-echo "Building Caffe, MPI status: ${CAFFE_USE_MPI}"
-cd ../../caffe-action
-[[ -d build ]] || mkdir build
-cd build
-if [ "$CAFFE_USE_MPI" == "MPI_ON" ]; then
-OpenCV_DIR=../../../3rd-party/opencv-$version/build/ cmake .. -DUSE_MPI=ON -DMPI_CXX_COMPILER="${CAFFE_MPI_PREFIX}/bin/mpicxx" -DCUDA_USE_STATIC_CUDA_RUNTIME=OFF
-else
-OpenCV_DIR=../../../3rd-party/opencv-$version/build/ cmake .. -DCUDA_USE_STATIC_CUDA_RUNTIME=OFF
-fi
-if make -j32 install ; then
-    echo "Caffe Built."
-    echo "All tools built. Happy experimenting!"
-    cd ../../../
-else
-    echo "Failed to build Caffe. Please check the logs above."
-    exit 1
-fi
+#echo "Building Caffe, MPI status: ${CAFFE_USE_MPI}"
+#cd ../../caffe-action
+#[[ -d build ]] || mkdir build
+#cd build
+#if [ "$CAFFE_USE_MPI" == "MPI_ON" ]; then
+#OpenCV_DIR=../../../3rd-party/opencv-$version/build/ cmake .. -DUSE_MPI=ON -DMPI_CXX_COMPILER="${CAFFE_MPI_PREFIX}/bin/mpicxx" -DCUDA_USE_STATIC_CUDA_RUNTIME=OFF
+#else
+#OpenCV_DIR=../../../3rd-party/opencv-$version/build/ cmake .. -DCUDA_USE_STATIC_CUDA_RUNTIME=OFF
+#fi
+#if make -j32 install ; then
+#    echo "Caffe Built."
+#    echo "All tools built. Happy experimenting!"
+#    cd ../../../
+#else
+#    echo "Failed to build Caffe. Please check the logs above."
+#    exit 1
+#fi
